e5fa0276156bf867b87552f3c3371cd044b1bb50
Sarthak Gupta <sarthakgupta259@gmail.com>
2018-03-24 00:32:30 +0530

    Wrapped expense summary in h1 tag

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -7,7 +7,7 @@ import expensesTotal from '../selectors/expenses-total';
 
 export const ExpenseSummary = ({ expenseCount, expensesTotal }) => (
   <div>
-    Viewing {expenseCount} {expenseCount===1?'expense':'expenses'} totalling {numeral(expensesTotal/100).format('$0,0.00')}
+    <h1>Viewing {expenseCount} {expenseCount===1?'expense':'expenses'} totalling {numeral(expensesTotal/100).format('$0,0.00')}</h1>
   </div>
 );
 
