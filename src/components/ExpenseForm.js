70ecf3aa403322b488bda1a0d55e56cee1ced2ee
Sarthak Gupta <sarthakgupta259@gmail.com>
2018-03-26 00:29:48 +0530

    Add Expensify styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -8,13 +8,13 @@ export default class ExpenseForm extends React.Component {
     this.state = {
       description: props.expense ? props.expense.description : '',
       note: props.expense ? props.expense.note : '',
-      amount: props.expense ? (props.expense.amount/100).toString() : '',
+      amount: props.expense ? (props.expense.amount / 100).toString() : '',
       createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
       calendarFocused: false,
       error: ''
     }
   }
-  
+
 
   onDescriptionChange = (e) => {
     const description = e.target.value;
@@ -34,7 +34,7 @@ export default class ExpenseForm extends React.Component {
   };
 
   onDateChange = (createdAt) => {
-    if(createdAt) {
+    if (createdAt) {
       this.setState(() => ({ createdAt }));
     }
   };
@@ -48,10 +48,10 @@ export default class ExpenseForm extends React.Component {
     if (!this.state.description || !this.state.amount) {
       this.setState(() => ({ error: 'Please provide description and amount.' }));
     } else {
-      this.setState(() => ({error: ''  }));
+      this.setState(() => ({ error: '' }));
       this.props.onSubmit({
         description: this.state.description,
-        amount: parseFloat(this.state.amount, 10)*100,
+        amount: parseFloat(this.state.amount, 10) * 100,
         createdAt: this.state.createdAt.valueOf(),
         note: this.state.note
       });
@@ -60,38 +60,41 @@ export default class ExpenseForm extends React.Component {
 
   render() {
     return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
-          <input
-            type="text"
-            placeholder="Description"
-            autoFocus
-            value={this.state.description}
-            onChange={this.onDescriptionChange}
-          />
-          <input
-            type="text"
-            placeholder="amount"
-            value={this.state.amount}
-            onChange={this.onAmountChange}
-          />
-          <SingleDatePicker
-            date={this.state.createdAt}
-            onDateChange={this.onDateChange}
-            focused={this.state.calendarFocused}
-            onFocusChange={this.onFocusChange}
-            numberOfMonths={1}
-            isOutsideRange={() => false}
-          />
-          <textarea
-            placeholder="Add a note for your expense (optional)"
-            value={this.state.note}
-            onChange={this.onNoteChange}
-          ></textarea>
-          <button>Add Expense</button>
-        </form>
-      </div>
+      <form className="form" onSubmit={this.onSubmit}>
+        {this.state.error && <p className="form__error">{this.state.error}</p>}
+        <input
+          type="text"
+          className="text-input"
+          placeholder="Description"
+          autoFocus
+          value={this.state.description}
+          onChange={this.onDescriptionChange}
+        />
+        <input
+          type="text"
+          className="text-input"
+          placeholder="amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        />
+        <SingleDatePicker
+          date={this.state.createdAt}
+          onDateChange={this.onDateChange}
+          focused={this.state.calendarFocused}
+          onFocusChange={this.onFocusChange}
+          numberOfMonths={1}
+          isOutsideRange={() => false}
+        />
+        <textarea
+          className="textarea"
+          placeholder="Add a note for your expense (optional)"
+          value={this.state.note}
+          onChange={this.onNoteChange}
+        ></textarea>
+        <div>
+          <button className="button">Save Expense</button>
+        </div>
+      </form>
     )
   }
 }
--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import expensesTotal from '../selectors/expenses-total';
 
 export const ExpenseSummary = ({ expenseCount, expensesTotal }) => (
-  <div>
-    <h1>Viewing {expenseCount} {expenseCount===1?'expense':'expenses'} totalling {numeral(expensesTotal/100).format('$0,0.00')}</h1>
+  <div className="page-header">
+  <div className="content-container">
+    <h1 className="page-header__title">Viewing <span>{expenseCount}</span> {expenseCount===1?'expense':'expenses'} totalling <span>{numeral(expensesTotal/100).format('$0,0.00')}</span></h1>
+    <div className="page-header__actions">
+      <Link className="button" to="/create">Add Expense</Link>
+    </div>  
+  </div>
   </div>
 );
 
